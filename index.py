b1eac45cbb551077f04c988258f6d6e69a56ecfb
MLopez <lopezjimenezmauro05@gmail.com>
2019-03-13 21:17:14 -0600

    python visualizer of the progress of the year

---
+++ index.py
@@ -0,0 +1,83 @@
+import calendar
+import datetime
+
+def getDates():
+
+    currentDateTime = datetime.datetime.now()
+    currentDay = currentDateTime.day 
+    currentMonth = currentDateTime.month
+    currentYear = currentDateTime.year
+    
+    return ({'currentDay':currentDay, 'currentMonth':currentMonth ,'currentYear':currentYear })
+
+def getNumberOfDaysElapsed(day, month, year):
+
+    totalOfDays = 0
+    cursor = 1
+
+    while (cursor < month):
+        totalOfDays += calendar.monthrange(year, cursor)[1]
+        cursor += 1
+    
+    totalOfDays += day
+
+    return totalOfDays 
+
+def getPercentOfYearElapsed(daysElapsed):
+
+    daysOfYear = 356 
+    percent = round(100 * daysElapsed / daysOfYear, 1)
+    return (percent)
+
+def showProgress(percent):
+    cursor = 1
+    fill = "."
+    fillPercent = "."
+    limitVar = "."
+    yearVar = ""
+    while (cursor < 100):
+
+        limitVar+= "."
+
+        if (cursor < percent):
+            fill += "*"
+        else:
+            if (cursor == 99):
+                fill +="."
+            else:
+                fill += " "
+
+        if(49 == cursor ):
+            fillPercent += str(percent)+'%'
+        
+        else:
+
+            if( not (48 <= cursor and cursor <= 52)):
+
+                if (cursor < percent):
+                    fillPercent += "*"
+                else:
+                    if (cursor == 99):
+                        fillPercent +="."
+                    else:
+                        fillPercent +=" "
+            
+        cursor+=1
+    
+    
+    yearVar+= limitVar + "\n"+fill +"\n"+fillPercent +"\n"+fillPercent +"\n"+fill +"\n"+limitVar + "\n"
+
+    
+    return (yearVar)
+
+
+def main():
+
+    dates = getDates()
+    numberOfDaysElapsed = getNumberOfDaysElapsed(dates['currentDay'], dates['currentMonth'],dates['currentYear'])
+    percent = getPercentOfYearElapsed(numberOfDaysElapsed)
+    progressVar = showProgress(percent)
+
+    print ("Date: " + str( dates['currentDay'] ) + '/' + str( dates['currentMonth'] ) + '/' + str( dates['currentYear'] ) + "\nThe percentage of the progress of the year is:" )
+    print(progressVar)
+main()
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Visualizer  of the year progress
+
+Time goes fast, and in less than you realize, the year could be about to finish. For that reazon I made the "visualizer of the year progress" who is a simple python script with UI in console that let us to know the percet of the year elapsed.
+
+## About the script 
+
+### Whats in?
+> Current date
+
+### Whats happend?
+
+> Discover the total days elapsed in the year till the date
+
+> Discover which percent of the year those days meaning 
+
+### Whats out?
+
+> A UI console var with the percent of the year elapsed. The "*" meaning the fill one, ant " " meaning the empty space. 
+
+
+## Librarys used 
+
+- Calendar 
+- Datetime 
+
+### Calendar 
+Allow us to know how many days have a month according the year by ".monthrange()" method
+
+### Datetime 
+Allow us to kno the current date by the ".datetime.now()" method
